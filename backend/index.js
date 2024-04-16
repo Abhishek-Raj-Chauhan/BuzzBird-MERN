68d39783568c2f71e0f226844e36573d1eede20b
Abhishek Raj Chauhan <100334669+Abhishek-Raj-Chauhan@users.noreply.github.com>
2024-04-16 05:59:58 +0530

    giter7

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,26 @@
 const ConnectToMongo = require("./db");
 const express = require("express");
-const cors = require('cors')
-const dotenv = require('dotenv')
+const cors = require('cors');
+const dotenv = require('dotenv');
 
 ConnectToMongo();
 
 const app = express();
-dotenv.config()
+dotenv.config();
 const port = process.env.PORT;
 
-//Available Routes
-app.use(express.json())
+// Available Routes
+app.use(express.json());
 
-app.use(cors());
-app.use('/api/auth',require('./routes/auth.js'))
-app.use('/api/notes',require('./routes/notes.js'))
-app.use('/api/chatmsg',require('./routes/chatmsg.js'))
+// Configure CORS to allow requests from specific origin
+const corsOptions = {
+  origin: 'https://cozynotes-mern-1.onrender.com'
+};
+app.use(cors(corsOptions));
+
+app.use('/api/auth', require('./routes/auth.js'));
+app.use('/api/notes', require('./routes/notes.js'));
+app.use('/api/chatmsg', require('./routes/chatmsg.js'));
 
 app.get("/", (req, res) => {
   res.send("Hello Abhishek!");
