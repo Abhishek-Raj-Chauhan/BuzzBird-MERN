4235d11c311c25f1ed114bc77d29ad37d597b737
Abhishek Raj Chauhan <100334669+Abhishek-Raj-Chauhan@users.noreply.github.com>
2024-04-17 10:05:02 +0530

    end

--- src/components/Login.js
+++ src/components/Login.js
@@ -254,14 +254,14 @@ const Login = () => {
                 <h1>Important Note!</h1>
                 <p>As i am on a free hosting plan, the server may temporarily spin down after 15 minutes of inactivity. It might take a few seconds to become active again. Please be patient for the initial load, subsequent interactions will be smooth.</p>
               </div>
-              <div className="closer" style={{display:"flex",height:'100%',justifyContent:'flex-start'}}>
+              <div className="closer" style={{display:"flex",height:'100%',justifyContent:'flex-start',width:'0rem'}}>
                 <button
                   type="button"
                   id="but4"
                   className="btn-close"
                   data-bs-dismiss="modal"
                   aria-label="Close"
-                  ref={cref3}
+                  ref={cref3} style={{margin:'-1rem -1rem'}}
                 ></button>
               </div>
             </div>
