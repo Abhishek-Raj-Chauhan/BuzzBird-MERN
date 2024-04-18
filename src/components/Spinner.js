fd4f120f27ea74732fc63e54b8617838902bc3f3
Abhishek Raj Chauhan <100334669+Abhishek-Raj-Chauhan@users.noreply.github.com>
2024-04-18 14:07:24 +0530

    end

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,17 +3,36 @@ import loading from "./loading.gif";
 
 const Spinner = () => {
   return (
-    <div
-      className="text-center"
-      style={{
-        position: "fixed",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        zIndex: "9999", // Ensure the spinner appears on top of everything
-      }}
-    >
-      <img src={loading} alt="loading" style={{ width: "100px" }} />
+    <div>
+      {/* Semi-transparent overlay */}
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          backgroundColor: "rgba(0, 0, 0, 0.5)", // Adjust opacity here
+          zIndex: 9998, // Place the overlay beneath the spinner
+        }}
+      ></div>
+      {/* Spinner */}
+      <div
+        className="text-center"
+        style={{
+          position: "fixed",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          zIndex: 9999, // Ensure the spinner appears on top of the overlay
+        }}
+      >
+        <img src={loading} alt="loading" style={{ width: "200px" }} />
+        <br />
+        <p className="tex" style={{ color: "white" }}>
+          whooosh you've reached the end..
+        </p>
+      </div>
     </div>
   );
 };
