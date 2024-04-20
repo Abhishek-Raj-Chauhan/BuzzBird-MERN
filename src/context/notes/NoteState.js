3671bf633c0ee64305b1e4f8007282761408c7fe
Abhishek Raj Chauhan <100334669+Abhishek-Raj-Chauhan@users.noreply.github.com>
2024-04-20 06:17:13 +0530

    yes

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -142,7 +142,7 @@ const NoteState = (props) => {
       edescription: currentNote.description,
       etag: currentNote.tag,
     });
-    console.log(note);
+    console.log(currentNote);
   };
 
   return (
